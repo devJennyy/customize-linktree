fbab2fbf9f758930f3fca2f1ed9f1f849d2dfd82
Jenny Pieloor <135243946+devJennyy@users.noreply.github.com>
2024-03-17 21:09:35 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,83 @@
+import { BsThreeDots } from 'react-icons/bs'
 import './App.css'
-import MainCard from './components/MainCard/MainCard'
+import { RiLinkedinFill, RiTwitterXFill } from 'react-icons/ri';
+import { FaFacebook, FaFacebookF, FaGithubAlt } from 'react-icons/fa';
+import { SiInstagram } from 'react-icons/si';
+import { ImWhatsapp } from 'react-icons/im';
 
 function App() {
+
   return (
-    <div className='w-full h-full bg-gradient-to-br from-indigo-900 to-slate-900'>
-      <MainCard />
-    </div>
-  )
+    <>
+      <div className="h-full w-full flex justify-center py-4 bg-[url('public/img/Background.png')] bg-cover">
+        <div className="flex flex-col justify-center items-center gap-[30px] h-full w-[450px] bg-[#161A42]/30 rounded-[24px] relative">
+          <img
+            src="public/img/Ornament.svg"
+            className="absolute top-0 w-full"
+          />
+          {/* Profile Image */}
+          <div className="flex justify-center mt-[-70px]">
+            <img
+              src="public/img/Avatar.png"
+              className="object-cover w-[130px] h-[130px] rounded-full"
+            />
+          </div>
+
+          {/* Title Name */}
+          <div className="text-white mt-[-10px]">
+            <h1 className="text-[24px] font-semibold mb-1">Jenny Pieloor</h1>
+            <p className="text-[15px] font-semibold">UI/UX DESIGNER</p>
+          </div>
+
+          {/* Icon Button Links */}
+          <div className="flex flex-row justify-center items-center gap-8 text-white mb-4">
+            <button className=" z-[1] hover:scale-[1.4] transition-all">
+              <FaFacebook size={23} />
+            </button>
+
+            <button className=" z-[1] hover:scale-[1.4] transition-all">
+              <SiInstagram size={23} />
+            </button>
+
+            <button className=" z-[1] hover:scale-[1.4] transition-all">
+              <RiTwitterXFill size={23} />
+            </button>
+
+            <button className=" z-[1] hover:scale-[1.4] transition-all">
+              <ImWhatsapp size={23} />
+            </button>
+          </div>
+
+          {/* Buttons */}
+          <div className="flex flex-col justify-center items-center gap-3">
+            <button className="text-white font-semibold text-[20px] border-2 w-[350px] h-[64px] border-[#495099] rounded-[16px] flex justify-between items-center px-[20px] hover:bg-[#495099] hover:scale-95 transition-all">
+              <img src="public/icons/Heart.svg" className="w-[18px] ml-[2px]" />
+              <p>Portfolio</p> <BsThreeDots size={18} />
+            </button>
+            <button className="text-white font-semibold text-[20px] border-2 w-[350px] h-[64px] border-[#495099] rounded-[16px] flex justify-between items-center px-[20px] hover:bg-[#495099] hover:scale-95 transition-all">
+              <RiLinkedinFill />
+              <p>LinkedIn</p> <BsThreeDots size={18} />
+            </button>
+            <button className="text-white font-semibold text-[20px] border-2 w-[350px] h-[64px] border-[#495099] rounded-[16px] flex justify-between items-center px-[20px] hover:bg-[#495099] hover:scale-95 transition-all">
+              <FaGithubAlt />
+              <p>Github</p> <BsThreeDots size={18} />
+            </button>
+          </div>
+
+          {/* Buy me coffee */}
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src="public/icons/Coffee.png"
+              className="w-[100px] mt-[60px]"
+            />
+            <p className="text-white font-semibold mt-[-20px]">
+              Buy me coffee :&gt;
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App
